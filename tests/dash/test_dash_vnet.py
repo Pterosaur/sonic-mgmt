d63bfbd952f7bd233b573ac7c167bd14cd7efbd9
Ze Gan <ganze718@gmail.com>
2023-11-01 10:02:30 +0800

    test
    Signed-off-by: Ze Gan <ganze718@gmail.com>

--- tests/dash/test_dash_vnet.py
+++ tests/dash/test_dash_vnet.py
@@ -119,6 +119,12 @@ class PTFSSAdapter(BaseTest):
         ptf.dataplane_instance.port_add("ens4f0np0", 0, 1)
         self.dataplane = ptf.dataplane_instance
 
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.kill()
+
 
 @pytest.fixture
 def ptf_ss_adapter():
