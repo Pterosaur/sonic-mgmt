686756abd159bc44bccb297abad69fb028a3f228
Dashuai Zhang <dashuaizhang@microsoft.com>
2025-04-28 12:56:34 +1000

    remove part of #15225 as it's being included incorrectly.

--- tests/conftest.py
+++ tests/conftest.py
@@ -2880,12 +2880,3 @@ def cli_namespace_prefix(request, selected_asic_index):
         return ''
     else:
         return f'-n {NAMESPACE_PREFIX}{selected_asic_index}'
-
-
-def pytest_collection_modifyitems(config, items):
-    # Skip all stress_tests if --run-stress-test is not set
-    if not config.getoption("--run-stress-tests"):
-        skip_stress_tests = pytest.mark.skip(reason="Stress tests run only if --run-stress-tests is passed")
-        for item in items:
-            if "stress_test" in item.keywords:
-                item.add_marker(skip_stress_tests)
