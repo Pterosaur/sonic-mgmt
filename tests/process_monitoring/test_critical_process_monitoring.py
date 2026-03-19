c11c5bd439146a6c1e76324312380c81c9791ae4
Janet Cui <janetcui@microsoft.com>
2026-03-19 00:21:57 +0000

    Merged PR 19868: [test_critical_process_monitoring]: skip otel process
    [test_critical_process_monitoring]: skip otel process since it would autorestart
    
    Signed-off-by: Janet Cui <janet970527@gmail.com>

--- tests/process_monitoring/test_critical_process_monitoring.py
+++ tests/process_monitoring/test_critical_process_monitoring.py
@@ -327,6 +327,9 @@ def get_expected_alerting_messages_supervisor(duthost, containers_in_namespaces)
                 # TODO: Should remove the following two lines once the issue was solved in the image.
                 if "gnmi" in container_name_in_namespace and critical_process == "gnmi-native":
                     continue
+                # Skip 'otel' process since it would autorestart
+                if "otel" in container_name_in_namespace and critical_process == "otel":
+                    continue
 
                 logger.info("Generating the regex of expected alerting message for process '{}' in container '{}'"
                             .format(critical_process, container_name_in_namespace))
